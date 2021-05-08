bc6a4f3a19c21601656d6b358823cd3e3e7b20fe
tiagoolivv <suubmundo@gmail.com>
2021-05-08 17:13:34 -0300

    chore: add fonts, colors, icon && splash

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
+import AppLoading from 'expo-app-loading';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
+import {
+  useFonts,
+  Roboto_500Medium,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
 import Navigation from './src/navigation';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_500Medium,
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,16 +1,17 @@
 const color = {
-  primary: '',
-  background: '',
-  text: { primary: '' },
-  error: '',
-  success: '',
+  primary: '#303030',
+  secondary: '#E83F5B',
+  background: '#FFFFFF',
+  placeholder: '#F6F6F6',
+  text: { primary: '#0A0909', secondary: '#595959', tertiary: '#AAB2AD' },
+  error: '#E83F5B',
+  success: '#04D361',
 };
 
 const font = {
-  medium: '',
-  regular: '',
-  bold: '',
-  semibold: '',
+  medium: 'Roboto_500Medium',
+  regular: 'Roboto_400Regular',
+  bold: 'Roboto_700Bold',
 };
 
 export { color, font };

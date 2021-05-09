0174c5bb2ab795cc6ee5a4224bda515aeae4ccad
tiagoolivv <suubmundo@gmail.com>
2021-05-09 09:02:21 -0300

    fix: tabbar navigation

--- src/navigation/app.navigation.tsx
+++ src/navigation/app.navigation.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome } from '@expo/vector-icons';
 
 import SignIn from '../screens/SignIn';
 import JobVacancies from '../screens/JobVacancies';
+
+import { color as colors, font } from '../constants';
 
 const Tab = createBottomTabNavigator();
 
 function AppNavigation() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      tabBarOptions={{
+        labelStyle: {
+          fontFamily: font.regular,
+        },
+        keyboardHidesTabBar: true,
+        activeTintColor: colors.primary,
+        inactiveTintColor: colors.text.tertiary,
+        adaptive: true,
+        labelPosition: 'beside-icon',
+      }}
+    >
       <Tab.Screen
         name="JobVacancies"
         component={JobVacancies}
         options={{
           title: 'Anúcios',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="plus-circle" size={20} color={color} />
+          ),
         }}
       />
       <Tab.Screen
@@ -21,6 +38,9 @@ function AppNavigation() {
         component={SignIn}
         options={{
           title: 'Entrar',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="user-circle" size={20} color={color} />
+          ),
         }}
       />
     </Tab.Navigator>

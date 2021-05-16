abafdef6bca6626b4fdbc8466ff812d9aec256da
tiagoolivv <suubmundo@gmail.com>
2021-05-16 15:22:11 -0300

    chore: add vacancy screen

--- src/screens/AddVacancy/index.tsx
+++ src/screens/AddVacancy/index.tsx
@@ -1,13 +1,71 @@
 import React from 'react';
 
-import { Text } from 'react-native';
+import { Container, Logo, Form } from './styles';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
 
 function AddVacancy() {
   return (
     <Container>
-      <Text>Adicionar vaga</Text>
+      <Logo>JobFinder - Anunciar</Logo>
+      <Form contentContainerStyle={{ alignItems: 'center' }}>
+        <Input
+          placeholder="Titulo"
+          icon="address-card"
+          returnKeyType="next"
+          keyboardType="default"
+          autoCorrect={false}
+        />
+        <Input
+          placeholder="Descrição"
+          icon="file"
+          returnKeyType="next"
+          keyboardType="default"
+          autoCorrect={false}
+        />
+        <Input
+          placeholder="E-mail"
+          icon="user"
+          returnKeyType="next"
+          keyboardType="default"
+          autoCorrect={false}
+        />
+        <Input
+          placeholder="Telefone"
+          icon="phone"
+          returnKeyType="next"
+          keyboardType="number-pad"
+          autoCorrect={false}
+        />
+        <Input
+          placeholder="Remuneração"
+          icon="money"
+          returnKeyType="next"
+          keyboardType="number-pad"
+          autoCorrect={false}
+        />
+        <Input
+          placeholder="Estado"
+          icon="globe"
+          returnKeyType="next"
+          keyboardType="default"
+          autoCorrect={false}
+        />
+        <Input
+          placeholder="Cidade"
+          icon="location-arrow"
+          returnKeyType="next"
+          keyboardType="default"
+          autoCorrect={false}
+        />
+        <Input
+          placeholder="Tipo"
+          icon="filter"
+          returnKeyType="done"
+          keyboardType="default"
+          autoCorrect={false}
+        />
+      </Form>
     </Container>
   );
 }
--- src/screens/AddVacancy/styles.ts
+++ src/screens/AddVacancy/styles.ts
@@ -1,7 +1,25 @@
 import styled from 'styled-components/native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import { color, font } from '../../constants';
 
 export const Container = styled.View`
   flex: 1;
+  background-color: ${color.background};
   align-items: center;
-  justify-content: center;
+  margin-top: ${getStatusBarHeight()}px;
+`;
+export const Logo = styled.Text`
+  font-family: ${font.bold};
+  font-size: ${wp(6)}px;
+  color: ${color.primary};
+  text-align: center;
+`;
+export const Form = styled.ScrollView`
+  width: 100%;
+  margin-top: ${hp(2)}px;
 `;
